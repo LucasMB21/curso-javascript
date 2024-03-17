6c6abd549519c39849adc6fa18b129bd367b6ec3
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-16 23:25:29 -0300

    update curso.js

---
+++ atividades/atividade08.js
@@ -0,0 +1,6 @@
+var vel = 18.2
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
