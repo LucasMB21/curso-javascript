e8967794fe07d52b7743d78500b61270d291ed2c
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-24 20:55:02 -0300

    update verificador de idade

--- atividades/ex15/script.js
+++ atividades/ex15/script.js
@@ -8,7 +8,44 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex');
         var idade = ano - Number(fano.value);
-        res.innerHTML = `Idade calculada: ${idade}`;
+        var genero = '';
+        var img = document.createElement('img');
+        img.setAttribute('id', 'foto');
+        if (fsex[0].checked) {
+            genero = 'Homem';
+            if (idade >=0 && idade <10) {
+                //Criança
+                img.setAttribute('src', 'foto-bebe-m.png');
+                } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-m.png');
+                } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'foto-adulto-m.png');
+                } else {
+                //Idoso
+                img.setAttribute('src', 'foto-idoso-m.png');
+                }
+            
+        } else if (fsex[1].checked) {
+            genero = 'Mulher';
+            if (idade >= 0 && idade <10) {
+                //Criança
+                img.setAttribute('src', 'foto-bebe-f.png');
+                } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-f.png');
+                } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'foto-adulto-f.png');
+                } else {
+                //Idoso
+                img.setAttribute('src', 'foto-idoso-f.png');
+                }
+        }
+        res.style.textAlign = 'center';
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+        res.appendChild(img);
     }
 
 }
