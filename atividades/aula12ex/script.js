9958bf76e1e3900649846535f4275f45f18583e2
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-24 13:49:17 -0300

    update

--- atividades/aula12ex/script.js
+++ atividades/aula12ex/script.js
@@ -1,1 +1,8 @@
-alert('Olá!');
+function carregar() {
+var msg = window.document.getElementById('msg');
+var msg = window.document.getElementById('imagem');
+var data = new Date();
+var hora = data.getHours();
+msg.innerHTML = `Agora são ${hora} horas.`
+
+}
---
+++ atividades/ex14/script.js
@@ -0,0 +1,8 @@
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var msg = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas.`
+    
+    }
