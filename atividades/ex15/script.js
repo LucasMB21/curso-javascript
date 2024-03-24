96390a5e1e548c8e38e07bf86a99ae18e20efc00
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-24 20:05:22 -0300

    update

---
+++ atividades/ex15/script.js
@@ -0,0 +1,14 @@
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.getElementById('txtano');
+    var res = document.querySelector('div#res');
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!');
+    } else {
+        var fsex = document.getElementsByName('radsex');
+        var idade = ano - Number(fano.value);
+        res.innerHTML = `Idade calculada: ${idade}`;
+    }
+
+}
