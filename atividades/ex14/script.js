ac203cdb110ce0d53ae8b8f8f9cbdf17aa27fe70
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-24 14:12:34 -0300

    update app.js

--- atividades/ex14/script.js
+++ atividades/ex14/script.js
@@ -1,8 +1,21 @@
 function carregar() {
     var msg = window.document.getElementById('msg');
-    var msg = window.document.getElementById('imagem');
+    var img = window.document.getElementById('imagem');
     var data = new Date();
-    var hora = data.getHours();
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var hora =20 //data.getHours();
     
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'fotomanha.png';
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+        img.src = 'fototarde.png';
+        document.body.style.background = '#b9846f'
+    } else {
+        //BOA NOITE!
+        img.src= 'fotonoite.png';
+        document.body.style.background = '#515154'
     }
+}
