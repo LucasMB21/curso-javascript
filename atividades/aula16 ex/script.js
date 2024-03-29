402060636fa57066e2a3ae9d58f5d1e04b767f45
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-29 18:59:04 -0300

    update ex 16

--- atividades/aula16 ex/script.js
+++ atividades/aula16 ex/script.js
@@ -2,10 +2,32 @@ function contar() {
     let ini = document.getElementById('txti');
     let fim = document.getElementById('txtf');
     let passo = document.getElementById('txtp');
+    let res = document.getElementById('res');
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados');
+        res.innerHTML = 'Impossível contar';
+        //window.alert('[ERRO] Faltam dados');
+        
     } else {
-        alert('TUDO OK!')
+        res.innerHTML = 'Contando: <br>';
+        let i = Number(ini.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+        if (p <= 0) {
+            window.alert('Passo Inválido! considerando PASSO 1');
+            p = 1;
+        }
+        if (i < f) {
+            //contagem crescente
+            for(let c = i;c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`;
+            }
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`;
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`;
     }
 }
